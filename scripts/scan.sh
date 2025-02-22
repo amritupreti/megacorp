8513b7a7dbfa915bc8f2e6db412513ed5e35a2cb
amritupreti <amritkumarupreti@gmail.com>
2025-02-22 19:07:04 +0545

    K: credits

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,12 @@
-# TODO: write the script
+echo "\n=========== Scanning for credit card numbers ========"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "=========== credit card scan complete =========="
+
+echo "\n=========== Scanning for social security numbers ========"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "=========== Social security scan complete =========="
+
+echo "\n=========== Scanning for phone numbers ========"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+grep -rE --color=always '\([0-9]{3}\) [0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "=========== Phone number scan complete =========="
